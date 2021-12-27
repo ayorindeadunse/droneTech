52aaaa2e95e9e16c2e0378277f6090ca0011381e
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2021-12-27 17:39:44 +0100

    initializing droneTech API

---
+++ src/main/java/com/example/droneTech/DroneTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.droneTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/droneTech/DroneTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.droneTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
