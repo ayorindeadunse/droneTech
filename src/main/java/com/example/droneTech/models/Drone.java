e4d3e9aced38e166e998efe2d772feda5bba94ce
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2021-12-27 18:14:33 +0100

    initialized constructor and added getters and setters to Drone class

--- src/main/java/com/example/droneTech/models/Drone.java
+++ src/main/java/com/example/droneTech/models/Drone.java
@@ -2,11 +2,53 @@ package com.example.droneTech.models;
 
 public class Drone {
     // Declare drone properties
-    private Long id;
+    private Long id; //auto increment. Identity column.
     private String serialNumber;
     private DroneModel droneModel;
     private int droneWeight;
     private Double batteryCapacity;
 
+    // add constructor
 
+    public Drone() {
+    }
+
+    public Drone(String serialNumber, DroneModel droneModel, int droneWeight, Double batteryCapacity) {
+        this.serialNumber = serialNumber;
+        this.droneModel = droneModel;
+        this.droneWeight = droneWeight;
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public DroneModel getDroneModel() {
+        return droneModel;
+    }
+
+    public void setDroneModel(DroneModel droneModel) {
+        this.droneModel = droneModel;
+    }
+
+    public int getDroneWeight() {
+        return droneWeight;
+    }
+
+    public void setDroneWeight(int droneWeight) {
+        this.droneWeight = droneWeight;
+    }
+
+    public Double getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(Double batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
 }
