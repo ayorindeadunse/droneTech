76118ee30e8d3babcb500872ebdc442b1aa4619f
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2021-12-27 17:58:42 +0100

    added the Drone class and Drone Model enum

---
+++ src/main/java/com/example/droneTech/models/Drone.java
@@ -0,0 +1,8 @@
+package com.example.droneTech.models;
+
+public class Drone {
+    // Declare drone properties
+    private Long id;
+    private String serialNumber;
+
+}
---
+++ src/main/java/com/example/droneTech/models/DroneModel.java
@@ -0,0 +1,5 @@
+package com.example.droneTech.models;
+
+public enum DroneModel {
+    Lightweight, Middleweight, Cruiserweight, Heavyweight;
+}
