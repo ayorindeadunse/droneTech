be85e7a7b85db69a23fdc0984a0e811dd48e5dc5
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2021-12-28 19:21:32 +0100

    updated EventLog class

--- src/main/java/com/example/droneTech/models/EventLog.java
+++ src/main/java/com/example/droneTech/models/EventLog.java
@@ -1,10 +1,52 @@
 package com.example.droneTech.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "eventlog")
 public class EventLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String serialNumber;
+    private String droneState;
+    private int batteryLevel;
+    private Date dateCreated;
+    private Date dateModified;
 
+    public EventLog() {
+    }
+
+    public EventLog(String serialNumber, String droneState, int batteryLevel, Date dateCreated,Date dateModified) {
+        this.serialNumber = serialNumber;
+        this.droneState = droneState;
+        this.batteryLevel = batteryLevel;
+        this.dateCreated = dateCreated;
+        this.dateModified = dateModified;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getDroneState() {
+        return droneState;
+    }
+
+    public void setDroneState(String droneState) {
+        this.droneState = droneState;
+    }
+
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
 }
