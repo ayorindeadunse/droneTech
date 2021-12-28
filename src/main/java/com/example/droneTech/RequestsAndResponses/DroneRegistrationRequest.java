2254038caadc2be666f60723b254c64b3ba4d6d1
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2021-12-28 15:09:37 +0100

    updated DroneRegistrationRequest model

--- src/main/java/com/example/droneTech/RequestsAndResponses/DroneRegistrationRequest.java
+++ src/main/java/com/example/droneTech/RequestsAndResponses/DroneRegistrationRequest.java
@@ -7,8 +7,61 @@ public class DroneRegistrationRequest {
     @NotBlank
     @Size(max = 100)
     private String serialNumber;
+    @NotBlank
     @Size(max = 500)
     private int droneWeight;
+
+    private static AtomicLong idCounter = new AtomicLong();
+
+    @NotBlank
     private int batteryCapacity;
+
+    @NotBlank
     private String state;
+
+
+
+    public DroneRegistrationRequest() {
+    }
+
+    // Create unique serial number
+    public static String createSerialNumber()
+    {
+        return String.valueOf(idCounter.getAndIncrement());
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public int getDroneWeight() {
+        return droneWeight;
+    }
+
+    public void setDroneWeight(int droneWeight) {
+        this.droneWeight = droneWeight;
+    }
+
+    public int getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(int batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+
+
 }
