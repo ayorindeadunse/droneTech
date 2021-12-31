ca618ded86a8b2d1b09213e436805253ccce88b4
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2021-12-31 08:13:30 +0100

    Updated DroneController with registerdrone route

--- src/main/java/com/example/droneTech/controllers/DroneController.java
+++ src/main/java/com/example/droneTech/controllers/DroneController.java
@@ -1,7 +1,25 @@
 package com.example.droneTech.controllers;
 
+import com.example.droneTech.RequestsAndResponses.DroneRegistrationRequest;
+import com.example.droneTech.models.Drone;
+import com.example.droneTech.services.DroneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class DroneController {
+
+    @Autowired
+    private DroneService droneService;
+
+    @PostMapping("/registerdrone")
+public ResponseEntity Register(@RequestBody DroneRegistrationRequest drone)
+{
+    Drone d = droneService.registerDrone(drone);
+    return ResponseEntity.ok(d);
+}
 }
