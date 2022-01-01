b5e8a0262e6ec2f0cc4ce7ff74b01e98caddf8c9
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2022-01-01 18:18:58 +0100

    updated DroneRepository

--- src/main/java/com/example/droneTech/repositories/DroneRepository.java
+++ src/main/java/com/example/droneTech/repositories/DroneRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface DroneRepository extends JpaRepository<Drone,String> {
 
-    @Query(value = "SELECT SERIAL_NUMBER FROM EVENTLOG WHERE DRONE_STATE = 0", nativeQuery=true)
+    @Query(value = "SELECT DISTINCT SERIAL_NUMBER FROM EVENTLOG WHERE DRONE_STATE = 0", nativeQuery=true)
     List<String> getAllAvailableDrones();
 
     @Query(value = "SELECT MEDICINE_CODE FROM LOADEDDRONES WHERE SERIAL_NUMBER = ?1", nativeQuery=true)
@@ -20,7 +20,7 @@ public interface DroneRepository extends JpaRepository<Drone,String> {
     @Query(value ="SELECT DRONE_WEIGHT from DRONES WHERE SERIAL_NUMBER = ?1",nativeQuery = true)
     int getDroneWeight(String serialNumber);
 
-    @Query(value = "SELECT BATTERY_LEVEL from EVENTLOG WHERE SERIAL_NUMBER = ?1 ORDER BY DATE_CREATED desc LIMIT 1",nativeQuery = true)
+    @Query(value = "SELECT BATTERY_LEVEL from EVENTLOG WHERE SERIAL_NUMBER = ?1 ORDER BY DATE_CREATED DESC LIMIT 1",nativeQuery = true)
     int getDroneBatteryLevel(String serialNumber);
 
     @Query(value = "SELECT * FROM DRONES WHERE SERIAL_NUMBER = ?1",nativeQuery = true)
