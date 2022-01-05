d7a55ea80dceb00b8d00c33504381d313ba544df
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2022-01-05 02:31:33 +0100

    Added finishong touches

--- src/main/java/com/example/droneTech/controllers/DroneController.java
+++ src/main/java/com/example/droneTech/controllers/DroneController.java
@@ -23,22 +23,20 @@ public class DroneController {
     private DroneService droneService;
 
     @PostMapping("/api/droneTech/registerdrone")
-public ResponseEntity Register(@RequestBody DroneRegistrationRequest drone)
-{
-    //Check drone weight
-    if(drone.getDroneWeight() > Constants.DRONE_WEIGHT)
-    {
-        return ResponseEntity.badRequest().body(new ApiResponse(false,"Maximum Drone Weight is " + Constants.DRONE_WEIGHT,drone));
+    public ResponseEntity Register(@RequestBody DroneRegistrationRequest drone) {
+        //Check drone weight
+        if (drone.getDroneWeight() > Constants.DRONE_WEIGHT) {
+            return ResponseEntity.badRequest().body(new ApiResponse(false, "Maximum Drone Weight is " + Constants.DRONE_WEIGHT, drone));
+        }
+        Drone d = droneService.registerDrone(drone);
+        //return ResponseEntity.ok(d);
+        return ResponseEntity.ok(new ApiResponse(true, "Drone registered successfully!", d));
     }
-    Drone d = droneService.registerDrone(drone);
-    //return ResponseEntity.ok(d);
-   return ResponseEntity.ok(new ApiResponse(true, "Drone registered successfully!",d));
-}
 
-@PostMapping("/api/droneTech/loaddrone")
+    @PostMapping("/api/droneTech/loaddrone")
     public ResponseEntity LoadDrone(@RequestBody LoadDroneRequest loadDrone) throws DroneException {
-    List<LoadDrone> l = null;
-    //try {
+        List<LoadDrone> l = null;
+        //try {
         l = droneService.loadDrone(loadDrone);
         for (int i = 0; i < l.size(); i++) {
             if (l.get(i).getDroneState() == DroneState.IDLE) {
@@ -47,37 +45,43 @@ public ResponseEntity Register(@RequestBody DroneRegistrationRequest drone)
             return ResponseEntity.ok(new ApiResponse(true, "Drone Loaded Successfully!", l));
         }
 
-       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(false, "An Error occurred on the server", l));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(false, "An Error occurred on the server", l));
 
-}
+    }
 
-@PostMapping("/api/droneTech/registermedication")
-    public ResponseEntity registerMedication(@RequestBody MedicationRegistrationRequest medication)
-{
-    Medication m = droneService.registerMedication(medication);
-    //return ResponseEntity.ok(m);
-    return ResponseEntity.ok(new ApiResponse(true,"Medicine Successfully registered.",m));
-}
+    @PostMapping("/api/droneTech/registermedication")
+    public ResponseEntity registerMedication(@RequestBody MedicationRegistrationRequest medication) {
+        Medication m = droneService.registerMedication(medication);
+        if(m != null) {
+            return ResponseEntity.ok(new ApiResponse(true, "Medicine Successfully registered.", m));
+        }
+        return ResponseEntity.ok(new ApiResponse(false, "An error occurred", m));
+    }
 
-@PostMapping("/api/droneTech/getloadedmedication")
-    public ResponseEntity getLoadedMedication(@RequestBody GetMedicationRequest getMedicationRequest)
-    {
+    @PostMapping("/api/droneTech/getloadedmedication")
+    public ResponseEntity getLoadedMedication(@RequestBody GetMedicationRequest getMedicationRequest) {
         List<String> getMeds = droneService.getLoadedMedication(getMedicationRequest);
-       // return ResponseEntity.ok(getMeds);
-        return ResponseEntity.ok(new ApiResponse(true,"Medication Successfully  loaded",getMeds));
+        if (getMeds.size() != 0) {
+            return ResponseEntity.ok(new ApiResponse(true, "Medication Successfully  loaded", getMeds));
+        }
+        return ResponseEntity.ok(new ApiResponse(false, "No data retrieved", getMeds));
     }
     @GetMapping("/api/droneTech/getavailabledrones")
-    public ResponseEntity getAvailableDrones()
-    {
+    public ResponseEntity getAvailableDrones() {
         List<String> getDrones = droneService.getAvailableDrones();
-       // return ResponseEntity.ok(getDrones);
-        return ResponseEntity.ok(new ApiResponse(true,"Available Drones Successfully retrieved",getDrones));
+        // return ResponseEntity.ok(getDrones);
+        if (getDrones.size() != 0) {
+            return ResponseEntity.ok(new ApiResponse(true, "Available Drones Successfully retrieved", getDrones));
+        }
+        return ResponseEntity.ok(new ApiResponse(false, "No data retrieved", getDrones));
     }
+
     @PostMapping("/api/droneTech/getdronebatterylevel")
-    public ResponseEntity getDroneBatteryLevel(@RequestBody GetDroneBatteryLevelRequest getDroneBatteryLevelRequest)
-    {
+    public ResponseEntity getDroneBatteryLevel(@RequestBody GetDroneBatteryLevelRequest getDroneBatteryLevelRequest) {
         List<Integer> droneBatteryLevelResponse = droneService.getDroneBatteryLevel(getDroneBatteryLevelRequest);
-      //  return ResponseEntity.ok(droneBatteryLevelResponse);
-        return ResponseEntity.ok(new ApiResponse(true,"Current Drone Battery Level Returned",droneBatteryLevelResponse));
+        if (droneBatteryLevelResponse.size() != 0) {
+            return ResponseEntity.ok(new ApiResponse(true, "Current Drone Battery Level Returned", droneBatteryLevelResponse));
+        }
+        return ResponseEntity.ok(new ApiResponse(false, "No data retrieved", droneBatteryLevelResponse));
     }
 }
